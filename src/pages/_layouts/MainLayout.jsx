0c2b190ae7e496f5a8c4cdbecafadfdafe1f062b
adnanamiruddin <adnan.amiruddin34@gmail.com>
2024-01-09 22:58:27 +0800

    feat: Intall and configurate react toastify

--- src/pages/_layouts/MainLayout.jsx
+++ src/pages/_layouts/MainLayout.jsx
@@ -3,6 +3,8 @@ import { useDispatch } from "react-redux";
 import userApi from "@/api/modules/users.api";
 import Navbar from "@/components/layouts/Navbar";
 import { setUser } from "@/redux/features/userSlice";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function MainLayout({ children }) {
   const dispatch = useDispatch();
@@ -18,6 +20,16 @@ export default function MainLayout({ children }) {
 
   return (
     <div className={`min-h-screen bg-white`}>
+      <ToastContainer
+        position="bottom-left"
+        autoClose={4000}
+        theme="light"
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+      />
       <Navbar />
       <div className="container px-4 py-6 text-black">{children}</div>
     </div>
