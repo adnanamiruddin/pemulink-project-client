9a93d70e678050fa6f4481f853f5728c8d1749cd
adnanamiruddin <adnan.amiruddin34@gmail.com>
2024-01-11 19:21:15 +0800

    feat: Create globalLeadingSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./features/userSlice";
+import globalLoadingSlice from "./features/globalLoadingSlice";
 
 const store = configureStore({
   reducer: {
     user: userSlice,
+    globalLoading: globalLoadingSlice,
   },
 });
 
