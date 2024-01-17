a5ae5986da0b388aaf55d839f96f351a80ddcb10
adnanamiruddin <adnan.amiruddin34@gmail.com>
2024-01-17 21:11:56 +0800

    feat: Create component AvatarItem

--- src/components/layouts/AvatarItem.jsx
+++ src/components/layouts/AvatarItem.jsx
@@ -1,26 +1,21 @@
+import Image from "next/image";
 
-
-export default function AvatarItem() {
-  return (                <button
-    key={i}
-    className={`bg-white px-1 py-2 w-40 h-40 rounded-lg border-[3px] ${
-      selectedAvatar?.id === avatar.id
-        ? "border-blue-400"
-        : " border-white"
-    }`}
-    onClick={() => {
-      setSelectedAvatar(avatar);
-      createTeamForm.setFieldValue("avatarId", avatar.id);
-    }}
-  >
-    <Image
-      src={avatar.avatarURL}
-      alt={avatar.name}
-      width={500}
-      height={500}
-      className="object-cover"
-    />
-    <p className="text-center">{avatar.name}</p>
-  </button>
-  )
+export default function AvatarItem({ data, isActive, onClick }) {
+  return (
+    <button
+      className={`bg-white px-1 py-2 w-40 h-40 rounded-lg border-[3px] ${
+        isActive ? "border-blue-400" : " border-white"
+      }`}
+      onClick={onClick}
+    >
+      <Image
+        src={data.avatarURL || data.characterURL}
+        alt={data.name}
+        width={500}
+        height={500}
+        className="object-cover"
+      />
+      <p className="text-center">{data.name}</p>
+    </button>
+  );
 }
--- src/pages/competition/[id]/index.jsx
+++ src/pages/competition/[id]/index.jsx
@@ -2,6 +2,7 @@ import avatarsApi from "@/api/modules/avatars.api";
 import teamsApi from "@/api/modules/teams.api";
 import Input from "@/components/functions/Input";
 import LoadingButton from "@/components/functions/LoadingButton";
+import AvatarItem from "@/components/layouts/AvatarItem";
 import CompetitionHeader from "@/components/layouts/CompetitionHeader";
 import CompetitionTeamNavbar from "@/components/layouts/CompetitionTeamNavbar";
 import { useFormik } from "formik";
@@ -99,27 +100,15 @@ export default function TeamDetail() {
             <label className="text-lg">Tema Tim</label>
             <div className="flex items-center overflow-x-scroll gap-2 mt-2">
               {avatars.map((avatar, i) => (
-                <button
+                <AvatarItem
                   key={i}
-                  className={`bg-white px-1 py-2 w-40 h-40 rounded-lg border-[3px] ${
-                    selectedAvatar?.id === avatar.id
-                      ? "border-blue-400"
-                      : " border-white"
-                  }`}
+                  data={avatar}
+                  isActive={selectedAvatar?.id === avatar.id}
                   onClick={() => {
                     setSelectedAvatar(avatar);
                     createTeamForm.setFieldValue("avatarId", avatar.id);
                   }}
-                >
-                  <Image
-                    src={avatar.avatarURL}
-                    alt={avatar.name}
-                    width={500}
-                    height={500}
-                    className="object-cover"
-                  />
-                  <p className="text-center">{avatar.name}</p>
-                </button>
+                />
               ))}
             </div>
           </div>
@@ -130,26 +119,14 @@ export default function TeamDetail() {
             </label>
             <div className="flex items-center overflow-x-scroll gap-2 mt-2">
               {characters.map((character, i) => (
-                <button
+                <AvatarItem
                   key={i}
-                  className={`bg-white p-1 w-40 rounded-lg border-[3px] ${
-                    createTeamForm.values.characterId === character.id
-                      ? "border-blue-400"
-                      : " border-white"
-                  }`}
+                  data={character}
+                  isActive={createTeamForm.values.characterId === character.id}
                   onClick={() => {
                     createTeamForm.setFieldValue("characterId", character.id);
                   }}
-                >
-                  <Image
-                    src={character.characterURL}
-                    alt={character.name}
-                    width={100}
-                    height={100}
-                    className="object-cover w-full h-full"
-                  />
-                  <p className="text-center text-sm mt-2">{character.name}</p>
-                </button>
+                />
               ))}
             </div>
           </div>
